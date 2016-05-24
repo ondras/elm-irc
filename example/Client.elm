94ce807580f7d71bcb1b1723ede99bd0e880441a
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2016-05-24 10:11:56 +0200

    examples

---
+++ example/Client.elm
@@ -0,0 +1,80 @@
+import Http
+import Html exposing (..)
+import Html.App exposing (program)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import String
+
+import Irc
+import Irc.Type as Type
+import Irc.Cmd
+
+type alias Model =
+  {
+    input : String,
+    messages : List String
+  }
+
+type Msg
+  = Input String
+  | Send
+  | IrcMsg Type.Msg
+
+url =
+  "ws://localhost:6667/?server=" ++ Http.uriEncode "irc.nomi.cz"
+
+commands = Irc.Cmd.commands url
+
+format msg =
+  case msg of
+    Type.Registered -> "Registered"
+    Type.Notice str -> str
+    Type.Unknown str -> str
+    _ -> ""
+
+onIrc msg model =
+  case Debug.log "onIrc" msg of
+    _ ->
+      ({model | messages = (format msg) :: model.messages}, Cmd.none)
+
+passToIrc msg (model, cmd) =
+  (model, Cmd.batch [cmd, Irc.pass url msg])
+
+update msg model =
+  case Debug.log "update" msg of
+    Input str ->
+      {model | input = str} ! [Cmd.none]
+
+    Send ->
+      let
+        tokens = String.split " " model.input
+        cmd = List.head tokens |> Maybe.withDefault ""
+        params = List.drop 1 tokens
+      in
+        {model | input = ""} ! [commands.raw cmd params]
+
+    IrcMsg msg ->
+      onIrc msg model |> (passToIrc msg)
+
+view : Model -> Html Msg
+view model =
+  div []
+    [
+      pre [] (List.map viewMessage (List.reverse model.messages)),
+      input [onInput Input, value model.input] [],
+      button [onClick Send] [text "Send"]
+    ]
+
+viewMessage msg =
+  p [] [ text msg ]
+
+user =
+  Type.User "tester" "tester" "Test Testovič"
+
+main =
+  program
+    { init = (Model "" [], commands.register user)
+    , view = view
+    , update = update
+    , subscriptions = always ((Sub.map IrcMsg) (Irc.listen url))
+    }
---
+++ example/Elmer.elm
@@ -0,0 +1,44 @@
+import Http
+import Html
+import Html.App
+
+import Irc
+import Irc.Type as Type
+import Irc.Cmd
+
+url =
+  "ws://localhost:6667/?server=" ++ Http.uriEncode "irc.nomi.cz"
+
+commands = Irc.Cmd.commands url
+
+onIrc msg =
+  case Debug.log "msg" msg of
+    Type.Query {from, to, text} ->
+      commands.query from.nick text
+
+    Type.Message {from, channel, text} ->
+      commands.message channel text
+
+    Type.Registered ->
+      commands.join "#testik"
+
+    _ ->
+      Cmd.none
+
+passToIrc msg cmd =
+  Cmd.batch [cmd, Irc.pass url msg]
+
+update msg x =
+  (x, onIrc msg |> (passToIrc msg))
+
+user =
+  Type.User "elmer" "elmer" "Elmer Elmerovič"
+
+main =
+  Html.App.program
+    {
+      init = (Nothing, commands.register user),
+      view = always (Html.text "..."),
+      update = update,
+      subscriptions = always (Irc.listen url)
+    }
